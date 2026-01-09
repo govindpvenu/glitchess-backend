f069bd39b6ee8fd54870a79c5da9b2417a09aaa0
govindpvenu <govindpvenu.txt@gmail.com>
2026-01-09 18:06:58 +0530

    fix: updating user show cors error

--- src/server.ts
+++ src/server.ts
@@ -25,7 +25,7 @@ const server = http.createServer(app)
 export const io = new Server(server, {
     cors: {
         origin: [env.CLIENT_URL, "http://localhost:3000"],
-        methods: ["GET", "POST", "PUT", "DELETE"],
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
         credentials: true,
     },
 })
@@ -37,7 +37,7 @@ app.use(morgan("dev"))
 app.use(
     cors({
         origin: [env.CLIENT_URL, "http://localhost:3000"],
-        methods: ["GET", "POST", "PUT", "DELETE"],
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
         credentials: true,
     })
 )
