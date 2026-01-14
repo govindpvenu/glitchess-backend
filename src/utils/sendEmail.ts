ecad231aee1a069d31489d6227b6d1df1cd61644
govindpvenu <govindpvenu.txt@gmail.com>
2026-01-14 13:03:46 +0530

    chore: migrated to resend from nodemailer

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,24 +1,16 @@
-const nodemailer = require("nodemailer")
+import { Resend } from "resend"
 import env from "./validateEnv"
+
+const resend = new Resend(env.RESEND_API_KEY)
+
 const sendEmail = async (email: string, generatedOTP: number): Promise<void> => {
     try {
-        const transporter = nodemailer.createTransport({
-            host: "smtp.gmail.com",
-            port: 465,
-            secure: true,
-            service: "Gmail",
-            auth: {
-                user: env.EMAIL_USER,
-                pass: env.EMAIL_PASS,
-            },
-        })
-        let mailOptions = {
-            from: env.EMAIL_USER,
+        await resend.emails.send({
+            from: env.RESEND_FROM,
             to: email,
-            subject: "Otp for registration is: ",
+            subject: "OTP for registration",
             html: "<h3>OTP for account verification is </h3>" + "<h1 style='font-weight:bold;'>" + generatedOTP + "</h1>",
-        }
-        await transporter.sendMail(mailOptions)
+        })
     } catch (error) {
         console.error(error)
     }
